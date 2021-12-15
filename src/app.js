b9230c7f0c0f2decac2cd4a7563c0cbe9302a2a5
midhunhk <2026817+midhunhk@users.noreply.github.com>
2021-12-15 10:31:30 -0500

    update

--- src/app.js
+++ src/app.js
@@ -14,12 +14,12 @@ const accounts = JSON.parse(accountData)
 const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 const users = JSON.parse(userData);
 
-app.get('/', (req, res) => res.render('index', { title: 'Index', accounts: accounts,}) )
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts,}) )
 
-app.get('/savings', (req, res) => res.render('savings', { account: accounts.savings }) )
-app.get('/checking', (req, res) => res.render('savings', { account: accounts.savings }) )
-app.get('/credit', (req, res) => res.render('savings', { account: accounts.savings }) )
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }) )
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }) )
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }) )
 
-app.get('/profile', (req, res) => res.render('profile', { user: users[0] }) )
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }))
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'))
